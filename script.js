ec0023a2e75b8eebbbb3a38545a60aaa1b493a71
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-10-07 18:59:07 -0700

    created initial ajax call

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
+    const cityEl = $(".city");
+    const tempEl = $(".temp");
+    const humidityEl = $(".humidity");
+    const windEl = $(".wind");
+    const uvIndexEl = $(".uv-index");
+
+    // let cityInputEl = $("#search-field").val();
+    let cityInputEl = "Seattle";
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=cda0734d46f3ec29600ebac5178a0156&q=" + cityInputEl;
+
+    // WHEN I search for a city
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        // THEN I am presented with current and future conditions for that city and that city is added to the search history
+    }).then(function(response) {
+        console.log(response);
+
+        let kelvinTemp = response.main.temp;
+        let fahrenheitTemp = (kelvinTemp - 273.15) * 1.80 + 32;
+
+        cityEl.text(response.name);
+        // add degree symbol
+        tempEl.text("Temperature: " + fahrenheitTemp + " F");
+        humidityEl.text("Humidity: " + response.main.humidity + " %");
+        // If I can switch it to imperial then it will be mph
+        windEl.text("Wind Speed: " + response.wind.speed + " m/s");
+        // uvIndexEl.text("UV Index: " + response.)
+        
+    })
 
 
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
+
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // WHEN I view the UV index
@@ -14,6 +42,8 @@ $(document).ready(function() {
 // THEN I am again presented with current and future conditions for that city
 // WHEN I open the weather dashboard
 // THEN I am presented with the last searched city forecast
+
+
 
 
 })
